a934cbe19aa217d70ad3f5650209234e1185d049
Cindy7878 <cindy000708@gmail.com>
2024-06-17 12:26:30 +0900

    Create Xcode Project

---
+++ Pikacho/Pikacho/PikachoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PikachoApp.swift
+//  Pikacho
+//
+//  Created by SOOKYUNG CHO on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct PikachoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
