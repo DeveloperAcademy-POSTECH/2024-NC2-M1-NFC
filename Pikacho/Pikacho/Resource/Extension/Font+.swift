9aff27c647eba86977952dc63c5fc93eecd37234
Cindy7878 <cindy000708@gmail.com>
2024-06-18 09:12:29 +0900

    [Design] #2 폰트 업데이트

---
+++ Pikacho/Pikacho/Resource/Extension/Font+.swift
@@ -0,0 +1,21 @@
+//
+//  Font+.swift
+//  Pikacho
+//
+//  Created by SOOKYUNG CHO on 6/18/24.
+//
+
+import SwiftUI
+
+extension Font{
+    static let PikachoHeadiline = Font.custom("SF-Pro-Text-Bold", size: 30)
+    
+    static let PikachoTitle = Font.custom("SF-Pro-Text-Bold", size: 16)
+    
+    static let PikachoButton = Font.custom("SF-Pro-Text-Regular", size: 16)
+    
+    static let PikachoBody = Font.custom("SF-Pro-Text-Regular", size: 12)
+    
+    static let PikachoBody2 = Font.custom("SF-Pro-Text-Regular", size: 10)
+
+}
