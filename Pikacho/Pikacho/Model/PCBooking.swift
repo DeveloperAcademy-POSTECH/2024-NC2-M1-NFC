222e7e8a0f79588d050f24a2a6ca66207060ee00
hajizn-pi <springlhj2@gmail.com>
2024-06-18 11:33:42 +0900

    [#3] 모델 추가

--- Pikacho/Pikacho/Model/PCBooking.swift
+++ Pikacho/Pikacho/Model/PCBooking.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+class PCBooking: Identifiable {
+    var id: UUID
+    var name: String
+    var perposeOfReservation: String?
+    var numberOfPeople: Int?
+    var password: Int
+//    var hoursOfUse:
+    var colorOfRow: PCColorList = .default
+
+    init(id: UUID, name: String, perposeOfReservation: String? = nil, numberOfPeople: Int? = nil, password: Int, colorOfRow: PCColorList) {
+        self.id = id
+        self.name = name
+        self.perposeOfReservation = perposeOfReservation
+        self.numberOfPeople = numberOfPeople
+        self.password = password
+        self.colorOfRow = colorOfRow
+    }
+}
--- Pikacho/Pikacho/Model/PCColorList.swift
+++ Pikacho/Pikacho/Model/PCColorList.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+enum PCColorList: Int, CaseIterable, Hashable, Codable {
+    case `default`
+    case timeTable1
+    case timeTable2
+    case timeTable3
+    case timeTable4
+    case timeTable5
+    case timeTable6
+    case timeTable7
+    case timeTable8
+    case timeTable9
+    case timeTable10
+    case timeTable11
+    case timeTable12
+    case timeTable13
+
+    var tabkeBackgroundColor: Color {
+        switch self {
+        case .default:
+            return Color(hex: 0xECECEC)
+        case .timeTable1:
+            return Color(hex: 0xF9E398)
+        case .timeTable2:
+            return Color(hex: 0xFBE5CE)
+        case .timeTable3:
+            return Color(hex: 0xFFCFD2)
+        case .timeTable4:
+            return Color(hex: 0xF2C0E6)
+        case .timeTable5:
+            return Color(hex: 0xCFB9F1)
+        case .timeTable6:
+            return Color(hex: 0xA4C3F2)
+        case .timeTable7:
+            return Color(hex: 0x90DAF3)
+        case .timeTable8:
+            return Color(hex: 0x91EBF4)
+        case .timeTable9:
+            return Color(hex: 0x99F5E0)
+        case .timeTable10:
+            return Color(hex: 0xB9FBBE)
+        case .timeTable11:
+            return Color(hex: 0xD4E6F4)
+        case .timeTable12:
+            return Color(hex: 0xFAF9CD)
+        case .timeTable13:
+            return Color(hex: 0xFCD1BE)
+        }
+    }
+}
