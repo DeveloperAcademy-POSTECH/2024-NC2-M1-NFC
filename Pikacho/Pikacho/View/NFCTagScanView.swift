02557f8feba68eb71624638fc9e4cdab000ef21b
hajizn-pi <springlhj2@gmail.com>
2024-06-19 21:00:15 +0900

    [#16] NFC 스캔 단계 점검 추가 + 타깃 추가

--- Pikacho/Pikacho/View/NFCTagScanView.swift
+++ Pikacho/Pikacho/View/NFCTagScanView.swift
@@ -61,6 +61,7 @@ struct NFCTagScanView: View {
         guard NFCNDEFReaderSession.readingAvailable else {
             alertMessage = "This device doesn't support tag scanning."
             showingAlert = true
+            print("NFC scanning not available on this device.")
             return
         }
 
@@ -84,6 +85,7 @@ class NFCDelegate: NSObject, NFCNDEFReaderSessionDelegate {
     }
 
     func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        print("NFC tag detected.")
         DispatchQueue.main.async {
             self.haveToNavigate = true
         }
@@ -91,7 +93,7 @@ class NFCDelegate: NSObject, NFCNDEFReaderSessionDelegate {
 
     func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
         if let readerError = error as? NFCReaderError {
-
+            print("NFC session invalidated with error")
             if readerError.code != .readerSessionInvalidationErrorFirstNDEFTagRead && readerError.code != .readerSessionInvalidationErrorUserCanceled {
                 DispatchQueue.main.async {
                     self.alertMessage = error.localizedDescription
